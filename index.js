293c054deec85b20a7e267e857469df6362e5d22
Prem Kumar Jangid <75199303+premkumar-github-cm@users.noreply.github.com>
2022-10-07 21:55:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function openmenu(){
+    document.getElementById('menu2').style.display='block'
+}
+
+function closemenu(){
+    document.getElementById('menu2').style.display='none'
+}
+
+
+function openmsg(){
+    document.getElementById('chats').style.display='block'
+}
+
+function closemsg(){
+    document.getElementById('chats').style.display='none'
+}
